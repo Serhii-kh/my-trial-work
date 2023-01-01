1e4dd831586f50de4ab0d423636ee66471ee5dbe
Serhii Naboka <drifterzzz@gmail.com>
2023-01-01 18:22:57 +0200

    счсчс
    чсч

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const Name = "Serhii";
+let age = 39;
+let boo = true;
+
+const header = document.querySelector("#header");
+const headerLinks = document.querySelectorAll(".header__nav-link");
+const heroBtn = document.querySelector("#hero-btn");
+
+console.log(headerLinks);
+
+function sayHello() {
+  let message = "Hello " + Name;
+  console.log(message);
+}
+
+sayHello();
+
+function Math(a, b) {
+  let result = a + b;
+  return result;
+}
+
+let sum = Math(22, 33);
+
+console.log(sum);
+
+window.addEventListener("scroll", checkScroll);
+
+document.addEventListener("DOMContentLoaded", checkScroll);
+
+function checkScroll() {
+  let scrollPos = window.scrollY;
+  if (scrollPos > 0) {
+    header.classList.add("pink");
+  } else {
+    header.classList.remove("pink");
+  }
+}
+
+heroBtn.addEventListener("click", function () {
+  console.log("clicked");
+});
+
+for (let navItem of headerLinks) {
+  navItem.addEventListener("click", function () {
+    console.log(navItem.text);
+  });
+}
