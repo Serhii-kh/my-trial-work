158aefcdaca89bb351b4cfc47e1c6caeb5da90ef
Serhii Naboka <drifterzzz@gmail.com>
2023-01-08 10:13:47 +0200

    ы
    вы

--- js/script.js
+++ js/script.js
@@ -68,19 +68,23 @@ while (clientCounter <= maxClients) {
 //   console.log('you have not costs')
 // }
 
-const totalSpent = 2000
-let payment = 500
-let discount
+// const totalSpent = 2000
+// let payment = 500
+// let discount
 
-if (totalSpent >= 100 && totalSpent <= 1000) {
-  discount = 0.02
-} else if (totalSpent > 1000 && totalSpent <= 5000) {
-  discount = 0.05
-} else if (totalSpent > 5000) {
-  discount = 0.1
-} else {
-  discount = 0
+// if (totalSpent >= 100 && totalSpent <= 1000) {
+//   discount = 0.02
+// } else if (totalSpent > 1000 && totalSpent <= 5000) {
+//   discount = 0.05
+// } else if (totalSpent > 5000) {
+//   discount = 0.1
+// } else {
+//   discount = 0
+// }
+
+// let paymentNew = payment - payment * discount
+// console.log(paymentNew)
+
+for (let i = 100; i >= 20; i -= 20) {
+  console.log(i)
 }
-
-let paymentNew = payment - payment * discount
-console.log(paymentNew)
